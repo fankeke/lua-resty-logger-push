6aa08cee76f8ecea25a0dd20d6b677f2ce6d329f
fankeke <382482175@qq.com>
2017-05-27 20:47:37 -0700

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+Name
+====
+
+lua-resty-logpush -- push openresty/ngx_lua access_log to remote log server. eg. syslog-ng or NSQD
+
+Description
+===========
+
+this code is mostly ported from [lua-resty-logger-socket](https://github.com/cloudflare/lua-resty-logger-socket).
+
+
---
+++ src/log.lua
@@ -0,0 +1,23 @@
+local logger = require "lua-resty-logger-push.lib.logger.socket"
+
+if not logger.initted() then
+    local ok, err = logger.init {
+        host = "127.0.0.1",
+        port = 8881,
+        flush_limit = 128,
+    }
+
+    if not ok then
+        ngx.log(ngx.ERR, "faild to init logger: ", err)
+        return
+    end
+end
+
+
+local bytes, err = logger.log(ngx.var.request_uri)
+if err then
+    ngx.log(ngx.ERR, "failed to logg: ", err)
+    return
+end
+
+ngx.log(ngx.INFO, "success push")
